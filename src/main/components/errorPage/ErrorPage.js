443777670e9f08e78034d66edd34a7d77b8fcadd
Rektovski <murmanishvili94@gmail.com>
2022-08-10 23:46:11 +0400

    Version 1.0.1
    error page was updated
    
    typing effect animation was updated.
    
    added countdown to the animation

--- src/main/components/errorPage/ErrorPage.js
+++ src/main/components/errorPage/ErrorPage.js
@@ -1,24 +1,44 @@
 import "../../styles/errorPageStyle.css";
 import errorPageBackground from "../../images/error2.jpg";
+import {useEffect, useState} from "react";
+
+let intervalId;
 
 export default function ErrorPage() {
+    const [countDown, setCountDown] = useState(7);
+
+    useEffect(()=>{
+        if(countDown>1) {
+            clearInterval(intervalId);
+            intervalId = setInterval(() => {
+                setCountDown((prevState) => prevState - 1)
+            }, 1000)
+        } else {
+            clearInterval(intervalId);
+            window.location.replace('http://localhost:3000')
+        }
+        console.log(countDown);
+    }, [countDown]);
+
     return (
-        <div className={'/*errorPageMainElementStyle text-center*/'}>
+        <div>
             <img
                 className={'errorPageBackground'}
                 src={errorPageBackground}
                 alt={'error page background foto'}
             />
-            <div className={'errorMessage css-typing'}>
-                <p>
-                    Flags Down! ...
-                </p>
-                <p>
-                    Error Code: 404 - Page not found ...
-                </p>
-                <p>
-                    Check the link ...
-                </p>
+            <div className={'errorMessage'}>
+                <div className={'css-typing'}>
+                    <p>
+                        Flags Down! ...
+                    </p>
+                    <p>
+                        Error Code: 404 - Page not found ...
+                    </p>
+                    <p>
+                        Fall back in {countDown}...
+                    </p>
+                </div>
             </div>
         </div>
     );
